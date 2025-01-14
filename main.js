5b0bc6eab010dc7a76fc0545d736d44b292941b8
Mahmoud97Ghannam321 <mahmouss2mofeed@gmail.com>
2025-01-14 14:09:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//alert("hellow")
+//document.querySelector(".table");
+//console.log(document.querySelector(".table"));
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
